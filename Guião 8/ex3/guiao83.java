39f792fe3608d3412471ae889b51cac348cbf001
André Mourato <Andr Mourato>
2016-12-27 03:39:05 +0000

    Adicionado versão melhorada do exercicio 8.3

--- Guião 8/ex3/guiao83.java
+++ Guião 8/ex3/guiao83.java
@@ -6,13 +6,15 @@
 import java.io.*;
 import java.util.*;
 public class guiao83 {
-	static int count=0;
-	static Scanner read = new Scanner(System.in);
+	
 	public static void main (String args[]) throws IOException {
 		
-		File fileIn = new File("numbers.txt");
-		Scanner scf = new Scanner(fileIn);
-		PrintWriter pwf = new PrintWriter("numbers.txt");
+		Scanner read = new Scanner(System.in);
+		File fileIn;
+		Scanner scf;
+		PrintWriter write = new PrintWriter("newList.txt");
+		int numEl=0; //numero de elementos do array
+		int numNewEl=0; //número de novos elementos (numeros contidos no ficheiro)
 		int option;
 		int num;
 		int[] arrayInt = new int[50];
@@ -37,106 +39,98 @@ public class guiao83 {
 			option=read.nextInt();
 			switch(option){
 				case 1:
-					lerSeq(arrayInt);
-					break;
-				case 2:
-					impSeq(arrayInt);
-					break;
-				case 3:
-					System.out.printf("O valor maximo da sequencia e: %d\n",max(arrayInt));
-					break;
-				case 4:
-					System.out.printf("O valor minimo da sequencia e: %d\n",min(arrayInt));
-					break;
-				case 5:
-					System.out.printf("O valor medio da sequencia e: %d\n",med(arrayInt));
-					break;
-				case 6:
-					par(arrayInt);
-					break;
-				case 7:
-					for(int i=0;scf.hasNextLine();i++){
-						String line = scf.nextLine();
-						arrayFile[i] = Integer.parseInt(line);
-						System.out.println(arrayFile[i]);
+					numEl=0;
+					boolean valid = true;
+					for(int i=0;valid==true;i++){
+						System.out.printf("Valor: ");
+						arrayInt[i]=read.nextInt();
+						if(arrayInt[i]==0) valid = false;
+						else numEl++;
 					}
 					break;
-				case 8:
-					do{
-						System.out.println("Numero: ");
-						num = Integer.parseInt(read.nextLine());
-						
-					}while(num!=0);
+				case 2:
+					for(int i=0;arrayInt[i]!=0;i++){
+						System.out.printf("arrayInt[%d] = %d\n",i,arrayInt[i]);
+					}
+					break;
+				case 3:
+					int max = arrayInt[0];
+					for(int i=0;i<numEl;i++){
+						if(arrayInt[i]>max){max=arrayInt[i];}
+					}
+					System.out.printf("O valor maximo da sequencia e: %d\n",max);
+					break;
+				case 4:
+					int min = arrayInt[0];
+					for(int i=0;i<numEl;i++){
+						if(arrayInt[i]<min){min=arrayInt[i];}
+					}
+					System.out.printf("O valor minimo da sequencia e: %d\n",min);
+					break;
+				case 5:
+					double soma=0, media;
+					for(int i=0;i<numEl;i++){
+						soma+=arrayInt[i];
+					}
+					System.out.printf("Soma: %.0f\n",soma);
+					System.out.printf("No. ELEMENTOS: %d\n",numEl);
+					media=soma/(numEl);
+					System.out.printf("O valor medio da sequencia e: %.2f\n",media);
+					break;
+				case 6:
+					int numPar=0;
+					for(int i=0;i<numEl;i++){
+						if(arrayInt[i]%2==0) numPar++;
+					}
+					if(numPar==numEl){ System.out.print("A sequencia e constituida por numeros pares\n");
 					
+					}else{
+						System.out.print("A sequencia nao e constituida por numeros pares\n");
+					}
+					break;
+				case 7:
+					System.out.printf("Nome do ficheiro que contem os numeros: ");
+					String nameFile = read.next();
+					fileIn = new File(nameFile);
+					checkFile(fileIn, nameFile);
+					scf = new Scanner(fileIn);
+					for(int i=0;scf.hasNextLine();i++){
+						arrayFile[i] = scf.nextInt();
+						numNewEl++;
+					}
+					System.out.printf("Numeros lidos com sucesso!\n");
+					break;
+				case 8:
+					for(int i=0;i<(numEl+numNewEl);i++){
+						arrayInt[numEl+i] = arrayFile[i];
+					}
+					numEl += numNewEl;
+					System.out.printf("Numeros adicionados com sucesso!\n");
 					break;
 				case 9:
-				
+					for(int i=0;i<numEl;i++){
+						write.println(arrayInt[i]);
+					}
+					write.close();
+					System.out.printf("Sequencia imprimida para o ficheiro!\n");
 					break;
-				
 				case 10:
-				
+					//esta opçao é necessaria para o bom funcionamento do programa
 					break;
 				default:
-					System.out.printf("OPÇAO INVALIDA!\n");
+					System.out.printf("OPCAO INVALIDA!\n");
 					break;
 			}
 		}while(option!=10);//TERMINA QUANDO E INTRODUZIDO 10
 	}
-	//Ler sequencia
-	public static void lerSeq(int[] array){
-		int i=0;
-		do{
-			System.out.printf("Valor: ");
-			array[i]=read.nextInt();
-			i++;
-			count++;
-		}while(array[i-1]!=0);
-		count--;
-	}
-	//Imprimir sequencia
-	public static void impSeq(int array[]){
-		for(int i=0;array[i]!=0;i++){
-			System.out.printf("array[%d] = %d\n",i,array[i]);
+	public static void checkFile(File fileIn, String nameFile){
+		if(!fileIn.exists()){
+			System.out.printf("ERROR: The file %s doesn't exist.\n",nameFile);
+			System.exit(1);
 		}
-	}
-	//Calcular maximo da sequencia
-	public static int max(int[] array){
-		int max = array[0];
-		for(int i=0;i<count;i++){
-			if(array[i]>max){max=array[i];}
-		}
-		return max;
-	}
-	//Calcular minimo da sequencia
-	public static int min(int[] array){
-		int min = array[0];
-		for(int i=0;i<count;i++){
-			if(array[i]<min){min=array[i];}
-		}
-		return min;
-	}
-	//Calcular a media da sequencia
-	public static int med(int[] array){
-		int soma=0, media;
-		
-		for(int i=0;i<count;i++){
-			soma+=array[i];
-		}
-		System.out.printf("Soma: %d\n",soma);
-		System.out.printf("Nº ELEMENTOS: %d\n",count);
-		media=soma/(count);
-		return media;
-	}
-	//Detetar se e uma sequencia par
-	public static void par(int[] array){
-		int numPar=0;
-		for(int i=0;i<count;i++){
-			if(array[i]%2==0) numPar++;
-		}
-		if(numPar==count){ System.out.print("A sequencia e constituida por numeros pares\n");
-		
-		}else{
-			System.out.print("A sequencia nao e constituida por numeros pares\n");
+		if(!fileIn.canRead()){
+			System.out.printf("ERROR: The file %s cannot be read.\n",nameFile);
+			System.exit(2);
 		}
 	}
 }
