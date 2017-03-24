6824771e95492786a7c5b6b2a946d442d1f3301f
André Mourato <Andr Mourato>
2017-03-24 11:07:55 +0000

    Adicionado exercício 6 do guião 6

--- programacao-2/guiao6/ex6/p66.java
+++ programacao-2/guiao6/ex6/p66.java
@@ -1,23 +1,46 @@
-import java.util.*;
 import static java.lang.System.*;
 
 public class p66 {
 	
+	static int P;
+	static float juro;
+		
 	public static void main (String[] args) {
 		
-		Scanner read = new Scanner(in);
-		out.print("M: ");
-		int M = read.nextInt();
-		out.print("T: ");
-		int T = read.nextInt();
-		out.print("P: ");
-		int P = read.nextInt();
-		/*
-		 * 
-		 * 	Não percebi a pergunta. Trata-se de um problema de interpretação e não
-		 * de um problema no desenvolvimento do algoritmo
-		 * 
-		 * */
+		if(args.length != 4){
+			out.println("USO: java -ea p66 <n> <M> <T> <P>");
+			exit(1);
+		}
+		
+		try{ //Verifica se todos os argumentos são válidos
+			for(String a : args)
+				Integer.parseInt(a);
+		}catch(Exception e){
+			out.println("USO: java -ea p66 <n> <M> <T> <P>");
+			exit(1);
+		}
+		
+		int N = Integer.parseInt(args[0]);
+		int M = Integer.parseInt(args[1]);
+		int T = Integer.parseInt(args[2]);
+		P = Integer.parseInt(args[3]);
+		
+		float div = (float)M;
+		juro = 1+(float)T/100;
+		
+		/* Método iterativo*/
+		for(int n = 1; n <= N;n++)
+			div = div * juro - P;
+		out.printf("(iterativo) d(%d) = %.10f \n", N, div);
+		
+		/*Método recursivo*/
+		out.printf("(recursivo) d(%d) = %.10f\n", N, getDiv(M, N));
+	}
+	
+	static float getDiv(int M, int n){
+		
+		if(n == 0) return M;
+		else return getDiv(M, n-1) * juro - P;
 	}
 }
 
