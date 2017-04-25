3ab14cb379e84de1566edbb05b1b4a436e968dc9
André Mourato <Andr Mourato>
2017-04-25 21:47:03 +0000

    Corrigido exercício 4 do guiãoTA

--- programacao-2/guiaoTA/E04/P2.java
+++ programacao-2/guiaoTA/E04/P2.java
@@ -5,12 +5,20 @@ public class P2 {
 	public static void main (String[] args) {
 		
 		for(String s : args){
-			out.printf("\"%s\" -> \"%s\"",s, invertString(s, s.length()-1));
+			out.printf("\"%s\" -> \"%s\"\n",s, invertString(s, "", 0));
 		}
 	}
 	
-	static String invertString(String s, int i){
-		if(i < 0) return "";
-		return s.charAt(i) + invertString(s, i-1);
+	static String invertString(String s, String numbers, int i){
+		if(i > s.length() - 1) return "";
+		char c = s.charAt(i);
+		if(Character.isDigit(c)){
+			numbers = c + numbers;
+			if(i == s.length()-1 || !Character.isDigit(s.charAt(i+1))){
+				return numbers + invertString(s, "", i + 1);
+			}
+			return invertString(s, numbers, i+1); 
+		}
+		else return c + invertString(s, "", i + 1);
 	}
 }
